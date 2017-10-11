69bf17abf43c4299babf342d8c922bc2556277e1
Berry Wang <berrywang@hust.edu.cn>
2017-10-12 00:02:41 +0800

    change class triBody to support variable planet numbers(completed)

--- testmain.f90
+++ testmain.f90
@@ -14,20 +14,26 @@ program main
     character(50)           ::  data_file, plot_file
     character(255)          ::  plot_title
     real*8, dimension(2)    ::  xrange, yrange
+    real*8                  ::  fps, movie_sec
     real*8                  ::  t0, t, h, delta_t
     real*8                  ::  err, tol
     type(triBody)           ::  state, iState
+    type(planet), allocatable     ::  temp_planet(:)
 
     !------------------------------------------------------------
     !------------------------------------------------------------
+
+    fps = 30
+    movie_sec = 4
+
 
     t0  = 0.0d0        ! 0.0d0
     h   = 0.001d0      ! 1.0d0/1000.0d0
-    tol = 1.0e-8
-
+    tol = 1.0e-8       ! relative tolerance
     delta_t = 0.01
-    N       = 10000     ! say, 100000
-    skip    = 1     ! say, 10, namely make a frame per skip iterations
+
+    skip    = int(1.d0 / (fps * delta_t))     ! say, 10, namely make a frame per skip iterations
+    N = int(fps * movie_sec * skip)
 
 !     iState%a%m = 1.0d0              ! mass1
 !     iState%a%x = [ 0.0d0, 1.0d0, 0.0d0 ]     ! coordinate1
@@ -40,19 +46,19 @@ program main
 !     iState%c%m = 0.0d0              ! mass3
 !     iState%c%x = [ 0.0d0, 0.0d0, 0.0d0 ]     ! coordinate3
 !     iState%c%v = [ 0.0d0, 0.0d0, 0.0d0 ]     ! velocity3
+    allocate(temp_planet(3))
+    temp_planet(1)%m = 1.0d0              ! mass1
+    temp_planet(1)%x = [ 0.0d0, sqrt(3.0d0), 0.0d0 ]     ! coordinate1
+    temp_planet(1)%v = [ -sqrt(1.d0/2.0d0), 0.0d0, 0.0d0 ]     ! velocity1
 
-    iState%a%m = 1.0d0              ! mass1
-    iState%a%x = [ 0.0d0, sqrt(3.0d0), 0.0d0 ]     ! coordinate1
-    iState%a%v = [ -sqrt(1.d0/2.0d0), 0.0d0, 0.0d0 ]     ! velocity1
+    temp_planet(2)%m = 1.0d0              ! mass2
+    temp_planet(2)%x = [ -1.d0, 0.d0, 0.0d0 ]     ! coordinate2
+    temp_planet(2)%v = [ sqrt(1.d0/2.0d0) * 0.5d0, -sqrt(1.d0/2.0d0) * sqrt(3.d0) / 2.d0, 0.0d0 ]     ! velocity2
 
-    iState%c%m = 1.0d0              ! mass2
-    iState%c%x = [ -1.d0, 0.d0, 0.0d0 ]     ! coordinate2
-    iState%c%v = [ sqrt(1.d0/2.0d0) * 0.5d0, -sqrt(1.d0/2.0d0) * sqrt(3.d0) / 2.d0, 0.0d0 ]     ! velocity2
-
-    iState%b%m = 1.0d0              ! mass3
-    iState%b%x = [ 1.d0, 0.d0, 0.0d0 ]     ! coordinate3
-    iState%b%v = [ sqrt(1.d0/2.0d0) * 0.5d0, sqrt(1.d0/2.0d0) * sqrt(3.d0) / 2.d0, 0.0d0 ]     ! velocity3
-
+    temp_planet(3)%m = 1.0d0              ! mass3
+    temp_planet(3)%x = [ 1.d0, 0.d0, 0.0d0 ]     ! coordinate3
+    temp_planet(3)%v = [ sqrt(1.d0/2.0d0) * 0.5d0, sqrt(1.d0/2.0d0) * sqrt(3.d0) / 2.d0, 0.0d0 ]     ! velocity3
+    iState = triBody(temp_planet)
 
     xrange  =   [ -5, 5 ]        ! [ -10, 10 ]
     yrange  =   [ -5, 5 ]        ! [ -10, 10 ]
@@ -77,13 +83,13 @@ program main
     k = 0
     err = 0
     call write_state(data_unit,t,err,state)
-    call plot_state(plot_unit,state,k,frame_digs,plot_title,xrange,yrange)
+!    call plot_state(plot_unit,state,k,frame_digs,plot_title,xrange,yrange)
     do i=1,N
         call rkf45(h,t,delta_t,state,err,tol)
         call write_state(data_unit,t,err,state)
         if ( mod(i,skip) == 0 ) then
             k = k+1
-            call plot_state(plot_unit,state,k,frame_digs,plot_title,xrange,yrange)
+!            call plot_state(plot_unit,state,k,frame_digs,plot_title,xrange,yrange)
         end if
     end do
 
