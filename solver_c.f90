605caa09c9f07453a80505755b410d59f90dd7a1
Berry Wang <berrywang@hust.edu.cn>
2017-10-21 19:48:11 +0800

    changed behaviours of the c interface in order to improve performance (cost of time dropped from about 22.9s to 18.9-ish seconds based on the sample time)

--- solver_c.f90
+++ solver_c.f90
@@ -1,38 +1,43 @@
-subroutine iter_step(h, t, delta_t, err, tol, p_num, p_m, p_x, p_v) bind(c, name='iter_step')
-    use, intrinsic :: ISO_C_BINDING
+module glue
     use tri_body_prob
     use solver
-    integer(c_int), intent(in) :: p_num
-    real(c_double), intent(inout) :: h, t, delta_t, err, tol, p_m(p_num), p_x(p_num, 3), p_v(p_num, 3)
-    real*16 :: hh, tt, ddtt, eerrrr, ttooll
-    type(planet) :: plist(p_num)
     type(triBody) :: state
-    integer :: i,j
-!    print *, h, t, delta_t, err, tol
-!    print *, p_num
-!    print *, p_m
-!    print *, p_x
-!    print *, p_v
-    hh = h
-    tt = t
-    ddtt = delta_t
-    eerrrr = err
-    ttooll = tol
-    do i=1, p_num
-        plist(i)%m = p_m(i)
-        plist(i)%x(:) = p_x(i,:)
-        plist(i)%v(:) = p_v(i,:)
-    end do
-    state = triBody(plist)
-    call rkf45(hh,tt,ddtt,state,eerrrr,ttooll)
-    do i=1, p_num
-        !p_m(i) = state%p(i)%m
-        p_x(i,:) = state%p(i)%x(:)
-        p_v(i,:) = state%p(i)%v(:)
-    end do
-    h = hh
-    t = tt
-    delta_t = ddtt
-    err = eerrrr
-    tol = ttooll
-end subroutine iter_step
+    real*16 :: gh, gt, gdt, gerr, gtol
+contains
+    subroutine init(h, t, delta_t, err, tol, p_num, p_m, p_x, p_v) bind(c, name='init')
+        use, intrinsic :: ISO_C_BINDING
+        integer(c_int), intent(in) :: p_num
+        real(c_double), intent(inout) :: h, t, delta_t, err, tol, p_m(p_num), p_x(p_num, 3), p_v(p_num, 3)
+        type(planet) :: plist(p_num)
+        integer :: i,j
+        gh = h
+        gt = t
+        gdt = delta_t
+        gerr = err
+        gtol = tol
+        do i=1, p_num
+            plist(i)%m = p_m(i)
+            plist(i)%x(:) = p_x(i,:)
+            plist(i)%v(:) = p_v(i,:)
+        end do
+        state = triBody(plist)
+    end
+    subroutine iter_step(h, t, delta_t, err, tol, p_num, p_m, p_x, p_v) bind(c, name='iter_step')
+        use, intrinsic :: ISO_C_BINDING
+        integer(c_int), intent(in) :: p_num
+        real(c_double), intent(inout) :: h, t, delta_t, err, tol, p_m(p_num), p_x(p_num, 3), p_v(p_num, 3)
+        type(planet) :: plist(p_num)
+        integer :: i,j
+        call rkf45(gh,gt,gdt,state,gerr,gtol)
+        do i=1, p_num
+            !p_m(i) = state%p(i)%m
+            p_x(i,:) = state%p(i)%x(:)
+            p_v(i,:) = state%p(i)%v(:)
+        end do
+        h = gh
+        t = gt
+        delta_t = gdt
+        err = gerr
+        tol = gtol
+    end subroutine iter_step
+end module
--- tribody.py
+++ tribody.py
@@ -9,6 +9,11 @@ solver.iter_step.argtypes = [
     POINTER(c_int),
     POINTER(c_double), POINTER(c_double), POINTER(c_double)
 ]
+solver.init.argtypes = [
+    POINTER(c_double), POINTER(c_double), POINTER(c_double), POINTER(c_double), POINTER(c_double),
+    POINTER(c_int),
+    POINTER(c_double), POINTER(c_double), POINTER(c_double)
+]
 p_num = 3
 p_m = np.empty(p_num, dtype=c_double)
 p_x = np.empty((p_num, 3), dtype=c_double)
@@ -27,10 +32,14 @@ t = c_double(0.0)
 delta_t = c_double(1.0 / 60.0)
 err = c_double(0.0)
 tol = c_double(1.0E-16)
-cur_time = time.time()
+cur_time = time.process_time()
+solver.init(h, t, delta_t, err, tol, c_int(p_num),
+            p_m.ctypes.data_as(POINTER(c_double)),
+            p_x.ctypes.data_as(POINTER(c_double)),
+            p_v.ctypes.data_as(POINTER(c_double)))
 for i in range(0, int(60.0 * 200 * 1 / 60.0 / h.value)):
     solver.iter_step(h, t, delta_t, err, tol, c_int(p_num),
                      p_m.ctypes.data_as(POINTER(c_double)),
                      p_x.ctypes.data_as(POINTER(c_double)),
                      p_v.ctypes.data_as(POINTER(c_double)))
-print(time.time() - cur_time)
+print(time.process_time() - cur_time)
